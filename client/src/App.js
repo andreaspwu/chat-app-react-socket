dfe0e9d9b8d503d4f8b61672f341176f89b8edcf
andreaspwu <90894256+andreaspwu@users.noreply.github.com>
2023-05-25 20:56:42 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client'
 import { useState } from 'react'
 import Chat from './Chat'
 
-const socket = io.connect('https://chat-server-prod.up.railway.app')
+const socket = io.connect('https://localhost:3001')
 
 function App() {
   const [username, setUsername] = useState('')
